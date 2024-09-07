3bf9db45eb7dae6c436cfffd24ad8126d5817219
jeiyanet <jeiya5934@proton.me>
2024-09-07 22:57:09 +0800

    feat: updated bootstrap

--- deploy/bootstrap.sh
+++ deploy/bootstrap.sh
@@ -7,4 +7,16 @@ fi
 sudo apt update
 sudo apt dist-upgrade -y
 sudo apt install -y ansible-core git
+
 sudo ansible-pull -U https://github.com/jeiyanet/desktop-setup.git
+
+flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+pipx install ansible-lint
+pipx install eyed3
+pipx install gallery-dl
+pipx install tldr
+pipx install yt-dlp
+
+sudo nala fetch
+sudo nala update
